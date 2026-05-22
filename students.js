eac8ac6abe3169cf85850057c81ac655a9f8a570
kassavi <kassavi2005@gmail.com>
2026-05-22 14:43:51 +0100

    Ajout du formulaire d'ajout d'étudiants

--- students.js
+++ students.js
@@ -7,3 +7,13 @@ function afficherEtudiants() {
     tbody.innerHTML += `<tr><td>${e.nom}</td><td>${e.prenom}</td><td>${e.note}</td></tr>`;
   });
 }
+document.getElementById('form-etudiant').addEventListener('submit', function(e) {
+  e.preventDefault();
+  etudiants.push({
+    nom: document.getElementById('nom').value,
+    prenom: document.getElementById('prenom').value,
+    note: document.getElementById('note').value
+  });
+  afficherEtudiants();
+  this.reset();
+});
